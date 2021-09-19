2d800ea3903e43cbfbd7d118a928245e2fd7ba23
Radhakrishna <radhakrishna.mca10@gmail.com>
2021-09-19 23:08:22 +0530

    vpc block added

--- main.tf
+++ main.tf
@@ -2,4 +2,13 @@ provider "aws" {
   region     = "us-west-2"
   access_key = "AKIAUCBQAUP72IVXCWV3"
   secret_key = "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"
+
 }
+
+resource "aws_vpc" "myvpc1" {
+ cidr_block = "10.0.0.0/16"
+  
+} 
+ tags = {
+    Name = "Testvpc"
+  }
